15bb60f903ebea6b48991822a94bd15ac6c40f6f
imamanjangra <imamanjangra@gmail.com>
2026-01-27 21:24:50 +0530

    correct typo in authorization

--- Backend/src/middlewares/auth.middlewares.js
+++ Backend/src/middlewares/auth.middlewares.js
@@ -4,7 +4,10 @@ import jwt from "jsonwebtoken"
 export const protect = async (req , res , next) => {
     let token;
 
-    if(req.headers.authorization && req.headers.authorization.startWith("Bearer")){
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ){
         try {
             token = req.headers.authorization.split(" ")[1];
             const decoded = jwt.verify(token , process.env.JWT_SECRET);
