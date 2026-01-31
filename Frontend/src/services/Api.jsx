eccf4f8743078b7cca8433055990d8199241bd7b
imamanjangra <imamanjangra@gmail.com>
2026-01-31 17:16:34 +0530

    update API base URL

--- Frontend/src/services/Api.jsx
+++ Frontend/src/services/Api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://naviotech-project-todo.onrender.com/api/v1",
   withCredentials: true,
 });
 
