654a2fe6e9849aafd0c3c677c812cca03c3145a3
imamanjangra <imamanjangra@gmail.com>
2026-01-31 17:28:22 +0530

    add Deploy url in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://naviotech-project-todo-1.onrender.com"
+    ],
     credentials: true,
   })
 );
